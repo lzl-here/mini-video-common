d926787b3d0892c436a8c53d3d7b8b6c422ec706
lzl <2106181304@qq.com>
2024-12-29 21:46:49 +0800

    feat(lzl): 修改repo时间类型

--- model/timemodel.go
+++ model/timemodel.go
@@ -1,7 +1,9 @@
 package model
 
+import "time"
+
 type TimeModel struct {
-	CreatedAt string `json:"created_at,omitempty" gorm:"column:created_at"`
-	UpdatedAt string `json:"updated_at,omitempty" gorm:"column:updated_at"`
-	DeletedAt string `json:"deleted_at,omitempty" gorm:"column:deleted_at"`
+	CreatedAt time.Time `json:"created_at,omitempty" gorm:"column:created_at"`
+	UpdatedAt time.Time `json:"updated_at,omitempty" gorm:"column:updated_at"`
+	DeletedAt time.Time `json:"deleted_at,omitempty" gorm:"column:deleted_at"`
 }

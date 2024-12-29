079b2491c129478f3911649425c8326374995d76
lzl <2106181304@qq.com>
2024-12-29 21:38:30 +0800

    feat(lzl): 修改user接口

--- model/timemodel.go
+++ model/timemodel.go
@@ -1,9 +1,7 @@
 package model
 
-import "time"
-
 type TimeModel struct {
-	CreatedAt time.Time `json:"created_at,omitempty" gorm:"column:created_at"`
-	UpdatedAt time.Time `json:"updated_at,omitempty" gorm:"column:updated_at"`
-	DeletedAt time.Time `json:"deleted_at,omitempty" gorm:"column:deleted_at"`
+	CreatedAt string `json:"created_at,omitempty" gorm:"column:created_at"`
+	UpdatedAt string `json:"updated_at,omitempty" gorm:"column:updated_at"`
+	DeletedAt string `json:"deleted_at,omitempty" gorm:"column:deleted_at"`
 }
